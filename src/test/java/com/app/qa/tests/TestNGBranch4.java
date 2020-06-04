01562bfc38260da23e9bba6ab2f2fbc5d8910bad
anush <anush@192.168.2.75>
2020-06-04 16:21:51 -0400

    "changes"

--- src/test/java/com/app/qa/tests/TestNGBranch4.java
+++ src/test/java/com/app/qa/tests/TestNGBranch4.java
@@ -4,7 +4,7 @@ public class TestNGBranch4 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("branch4");
+System.out.println("branch4jdfjghgkjh");
 	}
 
 }
