08cbe04c1cfa5050038443eb8b9b2a5ed6f2464e
anush <anush@192.168.2.75>
2020-06-04 16:09:47 -0400

    "changes updated"

--- src/test/java/Test/AdminLoginTest.java
+++ src/test/java/Test/AdminLoginTest.java
@@ -1,12 +1,9 @@
 package Test;
 
-import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import Com.Util.BaseClass;
-import Com.Util.DataDriven;
 import pages.AddCustomerdetails;
 import pages.AdminDashboard;
 import pages.AdminLogin;
---
+++ src/test/java/Test/TestBranch.java
@@ -0,0 +1,9 @@
+package Test;
+
+public class TestBranch {
+
+	
+	public static void main(String args[]) {
+		System.out.println("hello");
+	}
+}
